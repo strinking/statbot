1f39c5df5e86af9b47f9f3c3c3f9035533f25a93
Ammon Smith <ammon.i.smith@gmail.com>
2017-07-15 21:53:53 -0700

    Add tests.

--- statbot/range_test.py
+++ statbot/range_test.py
@@ -14,6 +14,57 @@ from range import *
 import unittest
 
 class TestNullRange(unittest.TestCase):
-    pass
+    def test_contains(self):
+        self.assertNotIn(0, NULL_RANGE)
+        self.assertNotIn(None, NULL_RANGE)
+        self.assertNotIn('a', NULL_RANGE)
 
+    def test_minmax(self):
+        self.assertIs(NULL_RANGE.min(), None)
+        self.assertIs(NULL_RANGE.max(), None)
+
+    def test_equals(self):
+        self.assertEqual(NULL_RANGE, NullRange())
+        self.assertNotEqual(NULL_RANGE, PointRange(0))
+        self.assertNotEqual(NULL_RANGE, None)
+
+    def test_bool(self):
+        self.assertFalse(NULL_RANGE)
+        self.assertFalse(NullRange())
+
+class TestPointRange(unittest.TestCase):
+    def test_contains(self):
+        r = PointRange(5)
+
+        self.assertIn(5, r)
+        self.assertNotIn(2, r)
+
+    def test_minmax(self):
+        r = PointRange(-2)
+
+        self.assertEqual(r.min(), r.max())
+        self.assertEqual(-2, r.min())
+
+    def test_equals(self):
+        r = PointRange('e')
+
+        self.assertEqual(PointRange('e'), r)
+        self.assertNotEqual(PointRange('a'), r)
+        self.assertNotEqual(NULL_RANGE, r)
+
+    def test_bool(self):
+        self.assertTrue(PointRange(0))
+        self.assertTrue(PointRange('beta'))
+
+class TestMisc(unittest.TestCase):
+    def test_types(self):
+        r = Range(0, 3)
+        m = MultiRange()
+        p = PointRange(-3)
+        n = NullRange()
+
+        self.assertIsInstance(r, AbstractRange)
+        self.assertIsInstance(m, AbstractRange)
+        self.assertIsInstance(p, AbstractRange)
+        self.assertIsInstance(n, AbstractRange)
 
