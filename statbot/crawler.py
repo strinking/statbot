b53d7d5bd7cb2701342a6d3f739d3e4ce46538b0
Ammon Smith <ammon.i.smith@gmail.com>
2017-07-17 10:06:23 -0700

    Use producer/consumer model for crawler.

--- statbot/crawler.py
+++ statbot/crawler.py
@@ -23,6 +23,7 @@ __all__ = [
 ]
 
 MESSAGE_BATCH_SIZE = 256
+ASYNC_QUEUE_SIZE   = 32
 
 class DiscordHistoryCrawler:
     __slots__ = (
@@ -30,7 +31,10 @@ class DiscordHistoryCrawler:
         'sql',
         'config',
         'logger',
+        'channels',
+        'latest',
         'progress',
+        'queue',
     )
 
     @staticmethod
@@ -44,6 +48,17 @@ class DiscordHistoryCrawler:
             return message
         return None
 
+    @staticmethod
+    def _find_chunk(latest, mrange):
+        current = latest
+        for range in reversed(mrange.ranges):
+            count = current - range.max()
+            if count >= 0:
+                return (current, min(count, MESSAGE_BATCH_SIZE))
+
+            current = range.min()
+        return (current, MESSAGE_BATCH_SIZE)
+
     def __init__(self, client, sql, config, logger=null_logger):
         self.client = client
         self.sql = sql
@@ -52,6 +67,7 @@ class DiscordHistoryCrawler:
         self.channels = {} # {channel_id : channel}
         self.latest = {} # {channel_id : Optional[Message]}
         self.progress = {} # {channel_id : MultiRange}
+        self.queue = asyncio.Queue(ASYNC_QUEUE_SIZE)
 
         self._load()
 
@@ -81,7 +97,8 @@ class DiscordHistoryCrawler:
         self.client.add_listener(self, 'on_guild_channel_delete')
         self.client.add_listener(self, 'on_guild_channel_update')
         self.client.loop.create_task(self.serialize())
-        self.client.loop.create_task(self.run())
+        self.client.loop.create_task(self.producer())
+        self.client.loop.create_task(self.consumer())
 
     async def serialize(self):
         # Delay first save
@@ -90,16 +107,22 @@ class DiscordHistoryCrawler:
         while True:
             filename = self.config['serial']['filename']
             if self.config['serial']['backup']:
-                if os.path.exists(filename):
-                    os.rename(filename, filename + '.bak')
+                self.logger.info(f"Backing up {filename}...")
 
+                if os.path.exists(filename):
+                    try:
+                        os.rename(filename, filename + '.bak')
+                    except Exception:
+                        self.logger.error(f"Error backing up {filename}!", exc_info=1)
+
+            self.logger.info(f"Serializing progress to {filename}...")
             with open(filename, 'wb') as fh:
                 pickle.dump(self.progress, fh)
 
             # Sleep until next save
             await asyncio.sleep(self.config['serial']['periodic-save'])
 
-    async def run(self):
+    async def producer(self):
         # Setup
         await self.client.wait_until_ready()
         await self._init_channels()
@@ -109,7 +132,7 @@ class DiscordHistoryCrawler:
             # dict of channels may change size during
             # an iteration
             for cid in tuple(self.progress.keys()):
-                # Do round-robin between all the channels.
+                # Do round-robin between all the channels
                 try:
                     channel = self.channels[cid]
                     mrange = self.progress[cid]
@@ -117,24 +140,33 @@ class DiscordHistoryCrawler:
                 except Exception:
                     self.logger.error(f"Error reading messages from channel id {cid}", exc_info=1)
 
-    def _find_chunk(self, latest, mrange):
-        current = latest
-        for range in reversed(mrange.ranges):
-            count = current - range.max()
-            if count >= 0:
-                return (current, min(count, MESSAGE_BATCH_SIZE))
-
-            current = range.min()
-        return (current, MESSAGE_BATCH_SIZE)
-
     async def _read(self, channel, mrange):
         latest_id = self.latest[channel.id]
         before_id, limit = self._find_chunk(latest_id, mrange)
         before = await self.client.get_message(before_id)
-        async for message in channel.history(limit=MESSAGE_BATCH_SIZE):
-            # TODO
-            _ingest_message(message)
-        mrange.add(Range(_, _))
+
+        self.logger.info(f"Reading through channel {channel.id} starting at {before_id} (limit: {limit})")
+        prev_id = before_id
+        messages = await channel.history(before=before, limit=limit).flatten()
+
+        await self.queue.push(messages)
+        self.logger.info(f"Queued {limit} messages for ingestion")
+
+        mrange.add(Range(messages[-1].id, before_id))
+
+    async def consumer(self):
+        while True:
+            messages = await self.queue.get()
+            self.logger.info("Got group of messages from queue")
+
+            try:
+                with self.sql.transaction() as trans:
+                    for message in messages:
+                        self.sql.insert_message(trans, message)
+            except Exception:
+                self.logger.error(f"Error writing message id {message.id}", exc_info=1)
+
+            await self.queue.task_done()
 
     async def on_guild_channel_create(self, channel):
         if not self._channel_ok(channel):
