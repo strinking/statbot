0c8a729909563dcd2ef776a47a1fe086b894f427
Ammon Smith <ammon.i.smith@gmail.com>
2017-09-28 16:32:38 -0700

    Copy config file as private read-only.

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ dest_dir=~statbot/repo
 mkdir -p "$dest_dir"
 cp -a "$repo_dir" "$dest_dir"
 chown -R statbot "$dest_dir"
-install -m644 "$1" "$dest_dir/config.json"
+install -m400 "$1" "$dest_dir/config.json"
 install -m644 "$repo_dir/misc/statbot.service" /etc/systemd/system/statbot.service
 
 systemctl daemon-reload
