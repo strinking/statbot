3dec6c441a5584dd0d8914079b30936f4ab1c68c
Ammon Smith <ammon.i.smith@gmail.com>
2017-09-28 00:50:07 -0700

    Add systemd service file.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 set -eu
 
+if [[ $# -ne 1 ]]; then
+	echo >&2 "Usage: $0 statbot-config.json"
+	exit 1
+fi
+
 repo_dir="$(dirname "$0")"
 dest_dir=~statbot/dev
 
+mkdir -p "$dest_dir"
 cp -a "$repo_dir" "$dest_dir"
 chown -R statbot "$dest_dir"
+install -m644 "$1" "$dest_dir/config.json"
 install -m644 "$repo_dir/misc/statbot.service" /etc/systemd/system/statbot.service
 
 systemctl daemon-reload
