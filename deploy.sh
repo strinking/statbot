05329bf0a3d034243767a14571a60af1ab7462f4
Ammon Smith <ammon.i.smith@gmail.com>
2017-09-29 01:10:40 -0700

    Add messages and fix pip invocation.

--- deploy.sh
+++ deploy.sh
@@ -13,12 +13,16 @@ mkdir -p "$dest_dir"
 cp -a "$repo_dir" "$dest_dir"
 install -m400 "$1" "$dest_dir/config.yaml"
 chown -R statbot:statbot "$dest_dir"
+echo "Installed source code to '$dest_dir'"
+
+python3.6 -m pip install -r "$repo_dir/requirements.txt"
+echo "Installed Python dependencies"
 
 install -m644 "$repo_dir/misc/statbot.service" /etc/systemd/system/statbot.service
 chown root:root /etc/systemd/system/statbot.service
-
-python3.6 -m pip install --requirements "$repo_dir/requirements.txt"
+echo "Installed systemd service"
 
 systemctl daemon-reload
 systemctl restart statbot.service
+echo "Started statbot systemd service"
 
